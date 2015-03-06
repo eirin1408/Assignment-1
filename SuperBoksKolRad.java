102bba9f9931f289e9990d0ffe39b57823f19648
eirin1408 <xtreem_cool@hotmail.com>
2015-03-06 14:36:43 +0100

    Create SuperBoksKolRad.java

---
+++ SuperBoksKolRad.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import javax.swing.JFileChooser;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class SuperBoksKolRad {
+
+	int rad;
+	int kolonne;
+	int boks;
+	int tall;
+	Brett b;
+}
